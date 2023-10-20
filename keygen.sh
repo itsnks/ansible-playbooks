d85b4efdbcaf9f1b59168ea3e954c72dfa59cb77
Nikesh <eminem.nikesh@gmail.com>
2023-10-20 19:06:06 +0545

    comments and formatting

--- keygen.sh
+++ keygen.sh
@@ -4,7 +4,8 @@
 ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
 
 for val in ansible-host node1 node2 node3; do
-sshpass -p 'vagrant' ssh-copy-id -o "StrictHostKeyChecking=no" vagrant@$val
+    echo "---------------COPYING KEY TO ${val^^} NODE---------------"
+    sshpass -p 'vagrant' ssh-copy-id -o "StrictHostKeyChecking=no" vagrant@$val
 done
 
 #project directory for ansible
