bd03e87dd0d084609cd217c1494d7f8a1aa86b4a
Nikesh <eminem.nikesh@gmail.com>
2023-10-20 19:01:32 +0545

    comments and formatting

--- keygen.sh
+++ keygen.sh
@@ -7,12 +7,17 @@ for val in ansible-host node1 node2 node3; do
 sshpass -p 'vagrant' ssh-copy-id -o "StrictHostKeyChecking=no" vagrant@$val
 done
 
+#project directory for ansible
 PROJECT_DIRECTORY="/home/vagrant/ansible_project/"
 mkdir -p $PROJECT_DIRECTORY
 cd $PROJECT_DIRECTORY
 
+#creating inventory files for all nodes
 echo -e "ansible-host\n\n[debian1]\nnode1\n\n[debian2]\nnode2\n\n[debian3]\nnode3" > inventory
 echo -e "[defaults]\ninventory = inventory" > ansible.cfg
+echo "-------------------Checking uptime------------------"
+echo
 
-
+#running adhoc command that checks uptime
 ansible all -i inventory -m "shell" -a "uptime"
+echo
