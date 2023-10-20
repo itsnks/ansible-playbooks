13ef6a2d8ab66e0bb37db90994a891d2051468cb
Nikesh <eminem.nikesh@gmail.com>
2023-10-20 18:24:07 +0545

    added node details to hosts file

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,6 @@
 #enabling password authentication
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl restart sshd
+
+#appending node names and ips to the hosts file
+echo -e "192.168.54.5 ansible-host ansible-host \n192.168.54.10 test1.demo.com node1 \n192.168.54.11 test2.demo.com node2 \n192.168.54.12 test3.demo.com node3" >> /etc/hosts
