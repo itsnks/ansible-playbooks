5bba14646c524e7c411c0565c3d5eee7d860d992
Nikesh <eminem.nikesh@gmail.com>
2023-10-20 18:26:56 +0545

    install necessary packages on all nodes

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,6 @@ sudo systemctl restart sshd
 
 #appending node names and ips to the hosts file
 echo -e "192.168.54.5 ansible-host ansible-host \n192.168.54.10 test1.demo.com node1 \n192.168.54.11 test2.demo.com node2 \n192.168.54.12 test3.demo.com node3" >> /etc/hosts
+
+#install necessary packages
+sudo apt update && sudo apt install -y curl net-tools curl wget python3-pip
