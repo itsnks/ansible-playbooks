1cfd878669505faef68ad7af4af74269e6913eb5
Nikesh <eminem.nikesh@gmail.com>
2023-10-20 18:50:48 +0545

    generating ssh keypair across all nodes

--- keygen.sh
+++ keygen.sh
@@ -1,1 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#generating ssh keypair
+ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
+
+for val in ansible-host node1 node2 node3; do
+sshpass -p 'vagrant' ssh-copy-id -o "StrictHostKeyChecking=no" vagrant@$val
+done
